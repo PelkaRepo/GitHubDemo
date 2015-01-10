58068eda77cbe728f88e647e544ec4feef8be146
Arnold <arnoldpelka@gmail.com>
2015-01-10 01:46:59 -0500

    Committing working and tested demo code to be pushed back for further deployment

---
+++ src/main/java/org/pelka/demo/gitdemo/main/Start.java
@@ -0,0 +1,41 @@
+/**
+ * This package holds the main method and is where the main thread
+ * will begin iteration
+ */
+package org.pelka.demo.gitdemo.main;
+
+import org.apache.log4j.*;
+
+/**
+ * The Start class is created as a static point from which to start project iteration
+ * for the sample project
+ * 
+ * @author Arnold Pelka
+ *
+ */
+public class Start {
+	final static Logger logger = Logger.getLogger(Start.class);
+
+	/**
+	 * This is the main method, which initializes the main thread instance.
+	 * This method will act as an orchestrator for the rest of the program
+	 * 
+	 * @param args Any number of command line string arguments
+	 */
+	public static void main(String[] args) {
+		helloWorld("HELLO WORLD");
+
+	}
+
+	/**
+	 * This method only returns a valid string, demonstrating a working interface
+	 */
+	public static String helloWorld(String msg) {
+		String output = new String();
+		
+		output = "|-------------" + msg + "!--------------|";
+		logger.info(output);
+		
+		return output;
+	}
+}
---
+++ src/main/java/org/pelka/demo/gitdemo/main/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * This is designed to be a simple test of the capabilities of a local GitHub repository.
+ * The actual code to be built will do mostly nothing useful, but will be rather a
+ * proof of concept of a working repository system.
+ * 
+ * This package holds artifacts required for running the main thread, including the
+ * main class.
+ * 
+ * @author Arnold Pelka
+ *
+ */
+package org.pelka.demo.gitdemo.main;
